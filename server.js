d24b7573136954ea7031480e9b74b0e7371d1430
cherrydub <cherrydub@gmail.com>
2023-11-01 11:26:19 +0000

    trying diff approach for using the db configs

--- server.js
+++ server.js
@@ -28,14 +28,15 @@ require("dotenv").config();
 
 const db = knex({
   client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false },
-  },
+  connection: process.env.DB_URL,
+  // {
+  //   host: process.env.DB_HOST,
+  //   user: process.env.DB_USER,
+  //   password: process.env.DB_PASS,
+  //   database: process.env.DB_NAME,
+  //   port: process.env.DB_PORT,
+  //   ssl: { rejectUnauthorized: false },
+  // },
 });
 
 // db.select("*")
